168f2fbf365c10f90d414deff972a05d24fee152
HYDRA5541 <44673348+HYDRA5541@users.noreply.github.com>
2020-03-31 20:14:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/animationinandroid/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.animationinandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtHelloWorld;
+    private TextView txtHiWorld;
+
+    private boolean isHelloWorldShowing=true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtHelloWorld =findViewById(R.id.txtHelloWorld);
+        txtHiWorld=findViewById(R.id.txtHiWorld);
+
+        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isHelloWorldShowing){
+
+                    txtHelloWorld.animate().alpha(0).setDuration(3000);
+                    txtHiWorld.animate().alpha(1).setDuration(3000);
+
+                    isHelloWorldShowing=false;
+
+                }else {
+                    txtHiWorld.animate().alpha(0).setDuration(3000);
+                    txtHelloWorld.animate().alpha(1).setDuration(3000);
+
+                    isHelloWorldShowing=true;
+                }
+            }
+        });
+    }
+}
